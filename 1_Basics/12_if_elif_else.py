a094b9bf14ea70bb23529858353b58d94830571f
Raghavendar Subramani (MS/EMT5-AS) <Subramani.Raghavendar@in.bosch.com>
2025-11-10 19:10:49 +0530

    Basics if elif for loop

---
+++ 1_Basics/12_if_elif_else.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the number"))
+print(f'Entered number is {x}')
+if x <= 5 :
+    print("x is less than or equal to 5")
+elif x > 5 and x <= 10:
+    print("x is greater than 5 and less than or equal to 10")
+else:
+    print("x is greater than 10")
--- 1_Basics/4_for_loop_practise.py
+++ 1_Basics/4_for_loop_practise.py
@@ -1,3 +1,32 @@
 for i in range(1,10):
     print(i)
 print("Done")
+
+for i in range(1,10):
+    print(i * i)
+
+for ch in "Hello World":
+    print(ch)
+
+for word in "Hello World".split():
+    print(word)
+
+# Excercises
+# Write a function to sum upto n
+
+def sumUptoN(number):
+   sum = 0
+   for i in range(1,number+1):
+       sum = sum + i
+   return sum
+
+print(sumUptoN(5))
+
+#Write a function to printNumberTriangle
+def triangleNumber(number):
+    for i in range(1,number+1):
+        for j in range(1, i+1):
+            print(f'{j} ')
+        print()
+
+triangleNumber(5)
